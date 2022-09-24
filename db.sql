114cb0a1fb32732908f2becb52468e88802e8945
unknown <2313661940@qq.com>
2022-09-24 20:15:36 +0800

    update

--- db.sql
+++ db.sql
@@ -7,9 +7,9 @@ use `local_search`;
 -- map
 create table `maps`(
    `id` int not null AUTO_INCREMENT comment "id" ,
-   'host' varchar(20) null,
-   'guest' varchar(20) null ,
+   `host` varchar(20) null,
+   `guest` varchar(20) null,
 
 -- 添加约束
-primary key (`id`)
+primary key (`id`),
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
